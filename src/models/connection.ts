624da9e6e7ba73ab44232825d921e94f7446ec57
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-11-07 15:24:58 -0300

    Agora vai

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -5,8 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default mysql.createPool({
-  host: process.env.DB_HOSTNAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOSTNAME || 'db',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_DATABASE || 'Trybesmith',
 });
